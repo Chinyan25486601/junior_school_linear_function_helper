a71fd8291383559c260a38db2452d32da99ea879
秦燕 <2640855495@qq.com>
2021-06-24 22:13:54 +0800

    在readme中添加了简要说明

--- src/app.py
+++ src/app.py
@@ -15,17 +15,13 @@ def sl(sender: ducky_cli, command, command_name):
     if len(command)!=4:
         sender.error(ERROR_MESSAGE_MODE.PARAM_ERROR)
         return
-
     sender.output(get_length_of_segment_from_two_points(Point(command[0],command[1]),Point(command[2],command[3])))
 
 def pf(sender: ducky_cli, command, command_name):
     if len(command)!=2:
         sender.error(ERROR_MESSAGE_MODE.PARAM_ERROR)
         return
-
-    rk=str((get_analytic_expression_of_directly_proportional_function_from_one_point(Point(command[0],command[1]))).evalf(5))
-    k=rk
-
+    k=str((get_analytic_expression_of_directly_proportional_function_from_one_point(Point(command[0],command[1]))).evalf(5))
     sender.output("k="+k)
     sender.output("y="+k+"x")
     pass
@@ -34,7 +30,9 @@ if __name__=="__main__":
     cli = ducky_cli(welcome_message="Welcome to function Helper!\nCopyright © 2021 Qinyan")
     cli.add_middle_ware(middle_ware)
     cli.add_command_rule("sl", sl)
-    cli.add_command_rule("segmentl_length", sl)
+    cli.add_command_rule("jl", sl)
+    cli.add_command_rule("segment_length", sl)
     cli.add_command_rule("pf",pf)
     cli.add_command_rule("proportional_function",pf)
+    cli.add_command_rule("zbl",pf)
     cli.mainloop()
